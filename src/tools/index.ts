419031108d940e719af939627a029c3adb7cf3d6
Himanshu-KRE <himanshu.nag@probestack.io>
2026-08-11 15:14:31 +0530

    Deploy banking mcp to Sandbox: src/tools/index.ts

---
+++ src/tools/index.ts
@@ -0,0 +1,9 @@
+// Re-export all tool handlers.
+export { post_customersHandler } from "./post_customers.js";
+export { get_customers_customer_idHandler } from "./get_customers_customer_id.js";
+export { post_accountsHandler } from "./post_accounts.js";
+export { get_accounts_account_idHandler } from "./get_accounts_account_id.js";
+export { get_accounts_account_id_balanceHandler } from "./get_accounts_account_id_balance.js";
+export { post_transactions_transferHandler } from "./post_transactions_transfer.js";
+export { get_transactions_transaction_idHandler } from "./get_transactions_transaction_id.js";
+export { post_auth_loginHandler } from "./post_auth_login.js";
